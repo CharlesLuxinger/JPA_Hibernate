e316a5399c1dd93ae49573e9f47b6137ef92779f
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-13 20:32:58 -0300

    4. Transient State

--- financas/src/br/com/alura/financas/teste/TesteConta.java
+++ financas/src/br/com/alura/financas/teste/TesteConta.java
@@ -7,14 +7,18 @@ import br.com.alura.financas.util.JPAUtil;
 
 public class TesteConta {
 	public static void main(String[] args) {
-		Conta conta = new Conta("Manuel", "123", "1234", "Quebrado");
+		Conta conta = new Conta("Manuela Santos", "123", "1234", "Quebrado");
 
 		EntityManager em = new JPAUtil().getEntityManager();
 
 		em.getTransaction().begin();
+		//A partir do persist a entidade passa de Transient > Managed sendo assim
+		//Qualquer alteração na entitidade conta no final da transação irá ser alterado no DB
 		em.persist(conta);
+		
+		conta.setBanco("Bradesco");
+		
 		em.getTransaction().commit();
-
 		em.close();
 	}
 }
