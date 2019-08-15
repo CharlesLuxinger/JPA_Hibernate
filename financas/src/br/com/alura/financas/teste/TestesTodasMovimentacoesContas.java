f8a61e7bf24881edb1c58b87fe514cc77a694f08
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-14 21:25:27 -0300

    14. Left Join

--- financas/src/br/com/alura/financas/teste/TestesTodasMovimentacoesContas.java
+++ financas/src/br/com/alura/financas/teste/TestesTodasMovimentacoesContas.java
@@ -16,7 +16,8 @@ public class TestesTodasMovimentacoesContas {
 		em.getTransaction().begin();
 
 		// Para evitar o comportamento Lazy(N+1 para cada select de conta 1 select de movimentacoes
-		// Add Join Fetch faz o select de movimentacoes junto como de conta
+		// Add Join Fetch(Inner Join) faz o select de movimentacoes junto como de conta
+		// Mas trás apenas as contas que possue movimentacao
 		String jpql = "select c from Conta c join fetch c.movimentacoes";
 		
 		Query query = em.createQuery(jpql);
@@ -31,6 +32,26 @@ public class TestesTodasMovimentacoesContas {
 		
 		em.getTransaction().commit();
 		em.close();
+		
+		
+		EntityManager em2 = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+
+		// é necessário add o left join para trazer tds as contas msm as que não possuem movimentacao
+		String jpql2 = "select c from Conta c left join fetch c.movimentacoes";
+		
+		Query query2 = em.createQuery(jpql2);
+		
+		List<Conta> todasContas2 = query2.getResultList();
+		
+		todasContas2.forEach(x ->{
+			System.out.println("Titular: " + x.getTitular());
+			System.out.println("Movimentaçoes: ");
+			System.out.println(x.getMovimentacoes());
+		});
+		
+		em2.getTransaction().commit();
+		em2.close();
 		
 	}
 }
